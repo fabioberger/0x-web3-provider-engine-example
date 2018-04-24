41cc00e56d1c2626a66cb0fdd6cb218b2e7c37a6
Fabio Berger <me@fabioberger.com>
2018-04-24 21:05:14 +0900

    Add web3 too

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
-import { MnemonicWalletSubprovider } from '@0xproject/subproviders';
+import { InjectedWeb3Subprovider } from '@0xproject/subproviders';
 import Web3ProviderEngine = require('web3-provider-engine');
+import * as Web3 from 'web3';
 
 const providerEngine = new Web3ProviderEngine();
 // Compose our Providers, order matters
 // Use the InjectedWeb3Subprovider to wrap the browser extension wallet
 // All account based and signing requests will go through the InjectedWeb3Subprovider
-providerEngine.addProvider(
-    new MnemonicWalletSubprovider({
-        mnemonic: 'concert load couple harbor equip island argue ramp clarify fence smart topic',
-    }),
-);
+const web3 = new Web3();
+providerEngine.addProvider(new InjectedWeb3Subprovider(web3.currentProvider));
 
 providerEngine.start();
